bb5df45fb1d8735f8f59b7f7e67c24d2cac786b6
Tillmann <heigel@gmino.de>
2013-09-16 09:55:01 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HRZPlan::Application.config.secret_key_base = '5f1749569c8f8054a0f08bbe223d4c887b0dd77c1dff65d06b19eb20837315f15d89938a69a720bf2142ed52c893471028db03e7863dc56fa20728f37f1dc416'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HRZPlan::Application.config.session_store :cookie_store, key: '_HRZ_Plan_session'
