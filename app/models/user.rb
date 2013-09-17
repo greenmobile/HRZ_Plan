d3d9432672764ecce5222f3c4e69ba2d45d471fa
Tillmann <heigel@gmino.de>
2013-09-17 09:47:20 +0200

    added active record user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,2 @@
-class User
-  attr_accessor :name, :email
-
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
-
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
+class User < ActiveRecord::Base
 end
