476462abe921841ff3fced3a271db902c7a289ea
Tillmann <heigel@gmino.de>
2013-09-18 16:57:56 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  
   before_save { self.email = email.downcase }
 
   validates :name, presence: true
@@ -6,4 +7,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
+ 
 end
